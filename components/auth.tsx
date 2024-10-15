50ae7f968eea6ac513f07059320e7ced67f9e8a1
JoelDiasCorreiaStefanini <e_jdcorreia@stefanini.com>
2024-10-15 00:42:37 -0300

    Auth Page (Preliminar)

---
+++ components/auth.tsx
@@ -0,0 +1,79 @@
+import { UserIcon } from "@heroicons/react/20/solid";
+import { ArrowRightIcon } from "@heroicons/react/20/solid";
+import { LockClosedIcon } from "@heroicons/react/20/solid";
+import Image from "next/image";
+
+const AuthComp = () => {
+  return (
+    <div>
+      <Image src={"/Vector 1.png"} alt="logo" height={300} width={200} className=""/>
+      <Image src={"/Vector 2.png"} alt="logo" height={200} width={90} className="absolute bottom-0"/>
+      
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <div className="w-full bg-white rounded-lg md:mt-0 sm:max-w-md xl:p-0 border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <div className="flex flex-col justify-center m-auto items-center mb-10">
+              <Image src={"/logo.png"} alt="logo" height={300} width={200} />
+              <h1 className="text-lg text-gray-900">Sign in to your account</h1>
+            </div>
+            <form className="space-y-4 md:space-y-6" action="#">
+              <div className="flex flex-row shadow-xl items-center p-2 rounded-full">
+                <UserIcon className="h-8 text-gray-500 ml-2"></UserIcon>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="bg-white text-gray-900 outline-none block w-full p-2.5 "
+                  placeholder="Username"
+                ></input>
+              </div>
+              <div className="flex flex-row shadow-xl items-center p-2 rounded-full">
+                <LockClosedIcon className="h-8 text-gray-500 ml-2"></LockClosedIcon>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-white text-gray-900 outline-none block w-full p-2.5"
+                ></input>
+              </div>
+              <div className="flex items-center justify-end">
+                <a
+                  href="#"
+                  className="text-sm font-medium text-gray-500 hover:underline"
+                >
+                  Forgot password?
+                </a>
+              </div>
+              <div className="flex flex-row justify-end mt-20">
+                <button
+                  type="submit"
+                  className="flex flex-row w-fit items-center content-center"
+                >
+                  <h2 className="text-xl w-40 font-bold">Sign in</h2>
+                  <div
+                    className="w-full text-white 
+                        bg-gradient-to-r from-teal-400 to-teal-900  
+                        rounded-full text-sm px-2 py-2.5 w-fit"
+                  >
+                    <ArrowRightIcon className="h-8 text-white ml-2"></ArrowRightIcon>
+                  </div>
+                </button>
+              </div>
+              <p className="text-sm font-light text-gray-500 mb-50">
+                Dont you have an account?{" "}
+                <a
+                  href="#"
+                  className="font-medium text-primary-600 hover:underline"
+                >
+                  Create
+                </a>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default AuthComp;
