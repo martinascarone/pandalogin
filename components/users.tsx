d02207b67d64ba4055c4d3b081929f2564959014
Pnda <pereiraleonel3126@gmail.com>
2024-10-15 22:20:43 -0300

    Users creados, creo que me faltó llamarlos en auth

---
+++ components/users.tsx
@@ -0,0 +1,9 @@
+// user.js
+const users = [
+    { username: 'Pedro', password: 'Pedrito43' },
+    { username: 'Ricardo', password: 'Ricardo52' },
+    { username: 'Estela', password: 'Estela96' },
+    { username: 'Marta', password: 'Marta75' },
+];
+
+export default users;
